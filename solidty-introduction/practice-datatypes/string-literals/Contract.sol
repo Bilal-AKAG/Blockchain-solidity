6ed1dd2cb998a23a7996258136233b149dfb71eb
Bilal Ali <bilal.ali.irp.dev@gmail.com>
2026-04-28 14:28:59 +0300

    add string literals ex

---
+++ solidty-introduction/practice-datatypes/string-literals/Contract.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+	bytes32 public msg1 = "Hello World";
+    string public msg2 = "Quite often long strings are stored seperately on other distributed services like IPFS, with a hash representation stored on the blockchain since storage on a blockchain is expensive";
+    
+}
